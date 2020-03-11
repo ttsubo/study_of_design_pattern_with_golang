bf94e29c48bcc856fffdc51aaf522156f0fce1de
Toshiki Tsuboi <t.tsubo2000@gmail.com>
2020-03-11 10:16:21 +0900

    Commandパターンのサンプル作成

---
+++ Command/Main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"strconv"
+
+	"./command"
+)
+
+func startMain(filename string, permission uint64) {
+	recv := &command.FileOperator{}
+	cc := &command.CompositeCommand{}
+	cc.AppendCmd(command.NewFileTouchCommand(filename, recv))
+	cc.AppendCmd(command.NewChmodCommand(filename, permission, recv))
+	cc.Execute()
+	cc.Display()
+
+}
+
+func main() {
+	flag.Parse()
+	perm32, _ := strconv.ParseUint(flag.Arg(1), 8, 32)
+	startMain(flag.Arg(0), perm32)
+}
---
+++ Command/command/command.go
@@ -0,0 +1,93 @@
+package command
+
+import (
+	"fmt"
+	"os"
+)
+
+type command interface {
+	execute()
+	display()
+}
+
+// FileOperator is struct
+type FileOperator struct {
+}
+
+func (f *FileOperator) createFile(filename string) {
+	os.Create(filename)
+}
+
+func (f *FileOperator) changeFileMode(filename string, permission uint64) {
+	os.Chmod(filename, os.FileMode(permission))
+}
+
+// CompositeCommand is struct
+type CompositeCommand struct {
+	cmds []command
+}
+
+// AppendCmd func for appending command
+func (c *CompositeCommand) AppendCmd(cmd command) {
+	c.cmds = append(c.cmds, cmd)
+}
+
+// Execute func for executing command
+func (c *CompositeCommand) Execute() {
+	for _, cmd := range c.cmds {
+		cmd.execute()
+	}
+}
+
+// Display func for executing command
+func (c *CompositeCommand) Display() {
+	for _, cmd := range c.cmds {
+		cmd.display()
+	}
+}
+
+// FileTouchCommand is struct
+type FileTouchCommand struct {
+	filename string
+	receiver *FileOperator
+}
+
+// NewFileTouchCommand func for initializing FileTouchCommand
+func NewFileTouchCommand(filename string, receiverObj *FileOperator) *FileTouchCommand {
+	return &FileTouchCommand{
+		filename: filename,
+		receiver: receiverObj,
+	}
+}
+
+func (f *FileTouchCommand) execute() {
+	f.receiver.createFile(f.filename)
+}
+
+func (f *FileTouchCommand) display() {
+	fmt.Printf("%% touch %s\n", f.filename)
+}
+
+// ChmodCommand is struct
+type ChmodCommand struct {
+	filename   string
+	permission uint64
+	receiver   *FileOperator
+}
+
+// NewChmodCommand func for initializing ChmodCommand
+func NewChmodCommand(filename string, permission uint64, receiverObj *FileOperator) *ChmodCommand {
+	return &ChmodCommand{
+		filename:   filename,
+		permission: permission,
+		receiver:   receiverObj,
+	}
+}
+
+func (c *ChmodCommand) execute() {
+	c.receiver.changeFileMode(c.filename, c.permission)
+}
+
+func (c *ChmodCommand) display() {
+	fmt.Printf("%% chmod %o %s\n", c.permission, c.filename)
+}
